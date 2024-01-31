3ff0acc91446d00589db03abdb78ab4710c0ddf5
Ángel Arciniegas <126428748+Asaa-dev@users.noreply.github.com>
2024-01-30 23:01:00 -0500

    Contact.html and css styles updated.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // TODO: Declaración de variables
 const navButton = document.querySelector('.nav__button');
 const navMenu = document.querySelector('.nav__container');
-const tabButttons = document.querySelectorAll('.services__tab');
+const tabButtons = document.querySelectorAll('.services__tab');
+const Networks__tabButtons = document.querySelectorAll('.networks__tab');
 
 const clickQuestions = document.querySelectorAll('.article__question');
+const contact_clickQuestions = document.querySelectorAll('.article_contact__question');
 
 // TODO: Evento de navegación responsive.
 navButton.addEventListener('click', ()=>{
@@ -40,14 +42,13 @@ const removeActiveElements = (selector)=>{
 }
 
 // TODO: Evento de navegación en section features.
-tabButttons.forEach(tabButton =>{
+tabButtons.forEach(tabButton =>{
     tabButton.addEventListener('click', (e)=>{
         e.preventDefault();
 
         if(!tabButton.classList.contains('services__tab--active')){
             
             const articleNumber = tabButton.getAttribute('data-article');
-
             const articleShow = document.querySelector(`.services__article:nth-of-type(${articleNumber})`);
 
             console.log(articleNumber);
@@ -55,9 +56,29 @@ tabButttons.forEach(tabButton =>{
             removeActiveElements('services__article--active');
             removeActiveElements('services__tab--active');
 
-
             articleShow.classList.add('services__article--active');
             tabButton.classList.add('services__tab--active');
+
+        }
+    })
+});
+
+Networks__tabButtons.forEach(Networks__tabButton =>{
+    Networks__tabButton.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        if(!Networks__tabButton.classList.contains('networks__tab--active')){
+            
+            const articleNumber = Networks__tabButton.getAttribute('data-article');
+            const articleShow = document.querySelector(`.networks__article:nth-of-type(${articleNumber})`);
+
+            console.log(articleNumber);
+
+            removeActiveElements('networks__article--active');
+            removeActiveElements('networks__tab--active');
+
+            articleShow.classList.add('networks__article--active');
+            Networks__tabButton.classList.add('networks__tab--active');
 
         }
     })
@@ -73,6 +94,19 @@ clickQuestions.forEach(clickQuestion =>{
         const anwserContainer = clickQuestion.nextElementSibling;
         
         anwserContainer.classList.toggle('article__content--show');
+
+    });
+});
+
+contact_clickQuestions.forEach(contact_clickQuestion =>{
+    contact_clickQuestion.addEventListener('click', ()=>{
+
+        const arrow = contact_clickQuestion.children[0];
+        arrow.classList.toggle('article_contact__arrow--rotate');
+
+        const anwserContainer = contact_clickQuestion.nextElementSibling;
+        
+        anwserContainer.classList.toggle('article_contact__content--show');
 
     });
 });
